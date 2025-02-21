9c021586d9f2e72e12355bf0736b5b15bffb1d45
24LP2879 24LP2879 <24LP2879@G304-CC0232.local>
2025-02-21 12:52:54 +0800

    added pic input

--- src/app/_components/cards/AddNewDishInfo/FoodImage.tsx
+++ src/app/_components/cards/AddNewDishInfo/FoodImage.tsx
@@ -1,4 +1,5 @@
 import { Textarea } from "@/components/ui/textarea";
+import { Image } from "lucide-react";
 
 export const FoodImage = () => {
   return (
@@ -9,10 +10,12 @@ export const FoodImage = () => {
       >
         Image
       </label>
-      <Textarea
-        placeholder="List ingredients..."
-        className="w-[100%] p-[8px 12px] text-wrap"
-      />
+      <div className="flex flex-col items-center justify-center gap-2 w-[100%] h-[138px] rounded-[6px] p-4 bg-[#2563EB33] bg-opacity-20 border border-dashed border-[rgba(37, 99, 235, 0.20)]">
+        <Image className="size-8 rounded-full p-2 bg-[#FFFFFF]" />
+        <p className="text-[14px] font-medium leading-5">
+          Choose a file or drag & drop it here
+        </p>
+      </div>
     </div>
   );
 };
