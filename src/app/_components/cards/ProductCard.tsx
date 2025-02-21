5955bc1be54af98eaf0e8607d858e5f0bc2e8bb4
altantogoo <82674692+ToKoHuber@users.noreply.github.com>
2025-02-21 23:28:26 +0800

    added button for card

--- src/app/_components/cards/ProductCard.tsx
+++ src/app/_components/cards/ProductCard.tsx
@@ -1,16 +1,20 @@
+import { Pencil } from "lucide-react";
 import Image from "next/image";
 
 export const ProductCard = ({ food }) => {
   console.log(food);
   return (
     <div className="w-[270.75px] h-[241px] p-4  flex flex-col gap-5 justify-center items-center border rounded-[20px] border-soid border-[#E4E4E7]">
-      <div className="overflow-hidden rounded-[12px] w-[238.75px] h-[129px]">
+      <div className="overflow-hidden rounded-[12px] w-[238.75px] h-[129px] relative">
         <Image
           src={food.image}
           width={238.75}
           height={129}
           alt="Dish Picture"
         />
+        <div className="size-[44px] p-[8px 16px] absolute bottom-5 right-5 bg-[#FFFFFF] rounded-full flex justify-center items-center">
+          <Pencil className="size-4" stroke="#EF4444" />
+        </div>
       </div>
 
       <div className="flex flex-col gap-2 w-[238.75px] ">
